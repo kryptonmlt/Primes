d437f764acb52d48d9f57c32a5b5602f92b8f88f
Nikos Ntarmos <nikos.ntarmos@glasgow.ac.uk>
2017-02-01 22:13:21 +0000

    Change site to use grunt-includes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,32 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-module.exports = function (grunt) {
-    // Project configuration.
-    grunt.initConfig({
-    });
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    // Remove built directory
+    clean: {
+      build: ['build/']
+    },
+
+    // Build the site using grunt-includes
+    includes: {
+      build: {
+        cwd: 'site',
+        src: [ '*.html', 'pages/*.html' ],
+        dest: 'build/',
+        options: {
+          flatten: true,
+          includePath: 'include',
+        }
+      }
+    }
+  });
+
+  // Load plugins used by this task gruntfile
+  grunt.loadNpmTasks('grunt-includes');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  // Task definitions
+  grunt.registerTask('build', ['clean', 'includes']);
+  grunt.registerTask('default', ['build']);
 };
