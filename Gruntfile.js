991db19d6b6feff704394cc71e3caf72c4143abb
Nikos Ntarmos <nikos.ntarmos@glasgow.ac.uk>
2017-02-02 00:47:32 +0000

    Use grunt-contrib-copy to copy over non-html files

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,12 +12,20 @@ module.exports = function(grunt) {
     includes: {
       build: {
         cwd: 'site',
-        src: [ '**/*.*' ],
+        src: [ '**/*.html' ],
         dest: 'build/',
         options: {
           flatten: false,
           includePath: 'include',
         }
+      }
+    },
+
+    copy: {
+      main: {
+         files: [
+            {expand: true, cwd: 'site', src: ['**/*.{jpg,gif,png,ico,xml,php,css,js,json,otf,eot,svg,ttf,woff,woff2}'], dest: 'build/'}
+         ]
       }
     }
   });
@@ -25,8 +33,9 @@ module.exports = function(grunt) {
   // Load plugins used by this task gruntfile
   grunt.loadNpmTasks('grunt-includes');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Task definitions
-  grunt.registerTask('build', ['clean', 'includes']);
+  grunt.registerTask('build', ['clean', 'includes', 'copy']);
   grunt.registerTask('default', ['build']);
 };
